52840c49b781c4097d43e954bff124154cfbc1c8
Rhys Previte <previterp88@gmail.com>
2021-04-02 17:17:23 -0500

    created file saving feature

--- other.py
+++ other.py
@@ -9,7 +9,7 @@ print(f'Filtering out {unfamiliar_skill}')
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml') 
 jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
-for job in jobs: 
+for idx, job in enumerate(jobs): 
     published = job.find('span', class_ = 'sim-posted').span.text
     if 'few' in published: 
         skills = job.find('span', class_ = "srp-skills").text.replace(' ','')
@@ -17,8 +17,8 @@ for job in jobs:
             if unfamiliar not in skills: 
                 company = job.find('h3', class_="joblist-comp-name").text.replace(' ','')
                 more_info = job.header.h2.a["href"]
-                print(f'Company Name: {company.strip()}')
-                print(f'Skills: {skills.strip()}')
-                print(f'More Info: {more_info}')
-                print("")
-
+                with open(f'posts/{str(idx) + company.strip()}.txt', 'w') as f:
+                    f.write(f'Company Name: {company.strip()} \n')
+                    f.write(f'Skills: {skills.strip()} \n')
+                    f.write(f'More Info: {more_info} \n')
+                print(f'File saved: {idx}')
