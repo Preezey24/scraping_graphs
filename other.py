3a3f72b669419592d82db9a6bd4919956239214f
Rhys Previte <previterp88@gmail.com>
2021-04-02 16:35:45 -0500

    scraping conditionals

--- other.py
+++ other.py
@@ -3,7 +3,20 @@ import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml') 
-job = soup.find('li', class_="clearfix job-bx wht-shd-bx")
-company = job.find('h3', class_="joblist-comp-name").text.replace(' ','')
-skills = job.find()
-print(company)
+jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+for job in jobs: 
+    company = job.find('h3', class_="joblist-comp-name").text.replace(' ','')
+    skills = job.find('span', class_ = "srp-skills").text.replace(' ','')
+    published = job.find('span', class_ = 'sim-posted').span.text
+    skip = False
+    for i in range(10): 
+        num = str(i)
+        if num in published: 
+            skip = True
+            break
+    if skip: 
+        continue 
+    print(f'''
+    Company: {company}
+    Required skills: {skills}
+    ''')
