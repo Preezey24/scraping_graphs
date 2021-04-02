086f6ca6fcadfd5bd63a02487ceef311b0a9dedf
Rhys Previte <previterp88@gmail.com>
2021-04-02 13:31:11 -0500

    soup playing

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ with open('home.html', 'r') as html_file:
     soup = BeautifulSoup(content, 'lxml')
     course_cards = soup.find_all('div', class_='card')
     for course in course_cards: 
-        print(course.h5)
+        course_name = course.h5.text
+        course_price = course.a.text
+
+        print(course_name)
+        print(course_price)
     
