5d8432419f8ed5260afc4001d543eafcb04a07f8
Rhys Previte <previterp88@gmail.com>
2021-04-02 16:22:03 -0500

    scraping

--- other.py
+++ other.py
@@ -4,4 +4,6 @@ import requests
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml') 
 job = soup.find('li', class_="clearfix job-bx wht-shd-bx")
-print(job)
+company = job.find('h3', class_="joblist-comp-name").text.replace(' ','')
+skills = job.find()
+print(company)
