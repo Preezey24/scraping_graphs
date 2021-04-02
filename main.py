07fdc91aca17cec7f8b7f93251b6afcace1ec6f5
Rhys Previte <previterp88@gmail.com>
2021-04-02 13:20:21 -0500

    added soup finding

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup
 with open('home.html', 'r') as html_file: 
     content = html_file.read() 
     
+    soup = BeautifulSoup(content, 'lxml')
+    courses = soup.find_all('h5')
+    
