fcc0bfc34c5ed09ae24d5b5eaead5e5e634c075d
Rhys Previte <previterp88@gmail.com>
2021-04-03 14:20:28 -0500

    grouping

--- pandas_pg.py
+++ pandas_pg.py
@@ -38,3 +38,9 @@ print(df.fillna(value="FILL VALUE"))
 print(df['A'].fillna(value=df['A'].mean()))
 
 #GROUPBY
+data = {'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+      'Person':['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+       'Sales':[200,120,340,124,243,350]}
+df = pd.DataFrame(data)
+byComp = df.groupby('Company')
+print(df.groupby('Company'))
