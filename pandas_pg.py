2329a037baafcb5cb2aa5c256c470602843724ba
Rhys Previte <previterp88@gmail.com>
2021-04-03 17:49:31 -0500

    calcs

--- pandas_pg.py
+++ pandas_pg.py
@@ -54,3 +54,12 @@ print(byComp.describe())
 df = pd.DataFrame({'col1':[1,2,3,4],'col2':[444,555,666,444],'col3':['abc','def','ghi','xyz']})
 print(df['col2'].value_counts())
 print(df[(df['col1']>2) & (df['col2']==444)])
+
+def times2(x): 
+    return x*2
+print(df['col1'].apply(times2))
+print(df['col1'].apply(lambda x: x * 2))
+print(df.sort_values('col2'))
+df.isnull()
+
+#DATA INPUT OUTPUT 
