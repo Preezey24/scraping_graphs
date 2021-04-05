3d2692515f721020f57161233ad638a378ff87f5
Rhys Previte <previterp88@gmail.com>
2021-04-05 16:02:49 -0500

    scraping

--- jobs/skills.py
+++ jobs/skills.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.linkedin.com/jobs/search/?alertAction=viewjobs&f_JT=F%2CC%2CI&f_TPR=a1617317976-&keywords=software%20engineer&savedSearchId=1213016336&searchAlertRefId=1724ba75-f511-46fe-93cf-a0d5befb7a32').text
+html_text = requests.get('https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&radius=25&ts=1615295423449&rq=1&rsIdx=1&fromage=last&newcount=720').text
 soup = BeautifulSoup(html_text, 'lxml')
-print(soup)
+jobs = soup.find_all('h2', class_="title")
+for job in jobs: 
+    print(job.a.text) 
+    print("")
