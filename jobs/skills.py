d13573c9c6078dd595ade7637132cb924a9c5070
Rhys Previte <previterp88@gmail.com>
2021-04-05 16:25:06 -0500

    scraping

--- jobs/skills.py
+++ jobs/skills.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&radius=25&ts=1615295423449&rq=1&rsIdx=1&fromage=last&newcount=720').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('h2', class_="title")
-for job in jobs: 
-    print(job.a.text) 
-    print("")
+for count in range(0,110,10): 
+    if not count: 
+        html_text = requests.get('https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&fromage=last&radius=25').text
+    else: 
+        html_text = requests.get(f'https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&fromage=last&radius=25&start={count}')
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('div', class_="jobSearch-SerpJobCard")
+    for job in jobs: 
+        print(job.h2.a.text) 
+        print("")
