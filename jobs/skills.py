2ecca2be109a7abde6643e67e7eca3bc4f375f3f
Rhys Previte <previterp88@gmail.com>
2021-04-05 16:36:36 -0500

    scraping

--- jobs/skills.py
+++ jobs/skills.py
@@ -5,9 +5,12 @@ for count in range(0,110,10):
     if not count: 
         html_text = requests.get('https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&fromage=last&radius=25').text
     else: 
-        html_text = requests.get(f'https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&fromage=last&radius=25&start={count}')
+        html_text = requests.get(f'https://ca.indeed.com/jobs?q=software+developer&l=Vancouver%2C+BC&fromage=last&radius=25&start={count}').text
     soup = BeautifulSoup(html_text, 'lxml')
-    jobs = soup.find_all('div', class_="jobSearch-SerpJobCard")
+    jobs = soup.find_all('h2', class_="title")
     for job in jobs: 
-        print(job.h2.a.text) 
+        print(jobs.a.text)
         print("")
+    # for job in jobs: 
+    #     print(job.h2.a.text) 
+    #     print("")
