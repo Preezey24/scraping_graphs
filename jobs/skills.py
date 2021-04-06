26db0c16ee9beba7af5d0c1fc5101fd39000e452
Rhys Previte <previterp88@gmail.com>
2021-04-05 20:53:11 -0500

    added flask

--- jobs/skills.py
+++ jobs/skills.py
@@ -6,6 +6,6 @@ for page in range(1,11):
     soup = BeautifulSoup(html_text, 'lxml')
     books = soup.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
     for book in books: 
-        print(jobs.a.text)
-        print("")
+        print(book.article.h3.text)
+    print(f'Page: {page}')
    
