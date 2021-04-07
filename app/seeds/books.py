b0d55415b604f6e7e22b7c57c68660d876a9d776
Rhys Previte <previterp88@gmail.com>
2021-04-07 16:18:26 -0500

    soup

--- app/seeds/books.py
+++ app/seeds/books.py
@@ -7,5 +7,7 @@ for page in range(1,11):
     books = soup.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
     for book in books: 
         print(book.article.h3.text)
+        print(book.find('div', class_='product_price').find('p', class_='price_color').text[2:])
+        print(book.article.div.p.text)
     print(f'Page: {page}')
    
