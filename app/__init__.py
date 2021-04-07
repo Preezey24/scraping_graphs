147ae8999b3588d4ea324cc5f9574c817f269e85
Rhys Previte <previterp88@gmail.com>
2021-04-07 16:47:35 -0500

    import trouble shooting

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask 
-from .config import Config
+from config import Config
+from models import db, Book
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+db.init_app(app)
+
+
