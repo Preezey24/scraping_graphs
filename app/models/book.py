3cbfc5043bc200ad3da714b9f002159e295cda5c
Rhys Previte <previterp88@gmail.com>
2021-04-07 16:30:36 -0500

    models

--- app/models/book.py
+++ app/models/book.py
@@ -1,2 +1,10 @@
 from .db import db
 
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.String(50), nullable=False)
+
